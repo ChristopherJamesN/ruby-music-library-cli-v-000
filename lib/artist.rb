e30a550eac45d6b0d8bff23e2d2a8631ef030f0c
Chris Nady <nady.christopher@gmail.com>
2017-09-14 19:01:03 +0000

    automatically backed up by learn

---
+++ lib/artist.rb
@@ -0,0 +1,76 @@
+class Artist
+
+  attr_accessor :name
+  @@song_count=0
+  @@all = []
+
+
+  extend Concerns::Findable
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def name
+    @name
+  end
+
+  def songs
+    @songs
+  end
+
+  def add_song(song)
+    if @songs.include?(song) == false
+      @songs << song
+      @@song_count += 1
+    end
+    if song.artist == nil
+      song.artist=self
+    end
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    @songs << song
+    song.artist = self
+    @@song_count += 1
+  end
+
+  def self.song_count
+    @@song_count
+  end
+
+  def save
+    @@all.push(self)
+  end
+
+  def self.all
+    @@all
+  end
+
+  def print_songs
+    @songs.each do |song|
+      puts "#{song.name}"
+    end
+  end
+
+  def self.destroy_all
+    @@all=[]
+  end
+
+  def self.create(name)
+    Artist.new(name).save[0]
+  end
+
+  def genres
+    genre_array = []
+    self.songs.each do |song|
+      if genre_array.include?(song.genre) == false
+        genre_array.push(song.genre)
+      end
+    end
+    genre_array
+  end
+
+end
---
+++ lib/genre.rb
@@ -0,0 +1,55 @@
+class Genre
+  attr_accessor :name
+  @@all=[]
+
+  extend Concerns::Findable
+
+  def initialize(name)
+    @name = name
+    @songs = []
+  end
+
+  def songs
+    @songs
+  end
+
+  def name
+    @name
+  end
+
+  def add_song(song)
+    if @songs.include?(song) == false
+      @songs << song
+    end
+    if song.genre == nil
+      song.genre = self
+    end
+  end
+
+  def artists
+    artists_array = []
+    self.songs.each do |song|
+      if artists_array.include?(song.artist) == false
+        artists_array.push(song.artist)
+      end
+    end
+    artists_array
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all=[]
+  end
+
+  def save
+    @@all.push(self)
+  end
+
+  def self.create(name)
+    Genre.new(name).save[0]
+  end
+
+end
